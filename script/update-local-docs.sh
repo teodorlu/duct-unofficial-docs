b57d7afbba3b477501ae36f1011559dd3cecde95
Teodor Heggelund <teodor@teodorheggelund.com>
2019-09-09 18:46:39 +0200

    Warn when building without local changes committed

--- script/update-local-docs.sh
+++ script/update-local-docs.sh
@@ -5,7 +5,10 @@ PROJECT="duct-unofficial-docs"
 VERSION="0.1.0-SNAPSHOT"
 PATH_HERE="$HOME/git/duct-unofficial-docs"
 
-echo "Please ensure that you've committed all your changes before running this."
+if [ "$(git status --porcelain)" ]; then
+    >&2 echo "You have uncommitted changes in your repository."
+    >&2 echo "Note that docs will be built from master, and ignores uncommitted changes."
+fi
 
 cd "$CLJDOC_ROOT" && "$CLJDOC_SCRIPT" ingest -p "$PROJECT"  -v "$VERSION" --git "$PATH_HERE"
 echo "View docs at http://localhost:8000/d/${PROJECT}/${PROJECT}/${VERSION}"
