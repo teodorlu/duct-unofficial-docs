f32fc98e48c21e19da02b88ab8901823902cc29a
Teodor Heggelund <teodor@teodorheggelund.com>
2019-09-09 13:57:59 +0200

    Fix readme

---
+++ script/update-local-docs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+CLJDOC_ROOT="$HOME/git/upstream/cljdoc"
+CLJDOC_SCRIPT="script/cljdoc"
+PROJECT="duct-unofficial-docs"
+VERSION="0.1.0-SNAPSHOT"
+PATH_HERE="$HOME/git/duct-unofficial-docs"
+
+cd "$CLJDOC_ROOT" && "$CLJDOC_SCRIPT" ingest -p "$PROJECT"  -v "$VERSION" --git "$PATH_HERE"
+echo "View docs at http://localhost:8000/d/${PROJECT}/${PROJECT}/${VERSION}"
