52d6dcada8e92f5c869aab68307ddcf8694b916b
Teodor Heggelund <teodor@teodorheggelund.com>
2019-09-09 13:59:06 +0200

    Notify the user to commit changes to git before

--- script/update-local-docs.sh
+++ script/update-local-docs.sh
@@ -5,5 +5,7 @@ PROJECT="duct-unofficial-docs"
 VERSION="0.1.0-SNAPSHOT"
 PATH_HERE="$HOME/git/duct-unofficial-docs"
 
+echo "Please ensure that you've committed all your changes before running this."
+
 cd "$CLJDOC_ROOT" && "$CLJDOC_SCRIPT" ingest -p "$PROJECT"  -v "$VERSION" --git "$PATH_HERE"
 echo "View docs at http://localhost:8000/d/${PROJECT}/${PROJECT}/${VERSION}"
