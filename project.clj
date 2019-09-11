9ba508ff0522ba62bab302aa7c391ae70919b2dc
Teodor Heggelund <teodor@teodorheggelund.com>
2019-09-11 12:43:51 +0200

    Link to Github

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject org.clojars.teodorlu/duct-unofficial-docs "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
+  :scm {:name "git" :url "https://github.com/teodorlu/duct-unofficial-docs"}
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
