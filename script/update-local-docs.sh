0e7f7989c442600d057e9c0778982a0b97ebc3b1
Teodor Heggelund <teodor@teodorheggelund.com>
2019-09-11 16:52:19 +0200

    Set correct group for project

--- script/update-local-docs.sh
+++ script/update-local-docs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 CLJDOC_ROOT="$HOME/git/upstream/cljdoc"
 CLJDOC_SCRIPT="script/cljdoc"
+GROUP="org.clojars.teodorlu"
 PROJECT="duct-unofficial-docs"
 VERSION="0.2.2-SNAPSHOT"
 PATH_HERE="$HOME/git/duct-unofficial-docs"
@@ -10,5 +11,5 @@ if [ "$(git status --porcelain)" ]; then
     >&2 echo "Note that docs will be built from master, and ignores uncommitted changes."
 fi
 
-cd "$CLJDOC_ROOT" && "$CLJDOC_SCRIPT" ingest -p "$PROJECT"  -v "$VERSION" --git "$PATH_HERE"
+cd "$CLJDOC_ROOT" && "$CLJDOC_SCRIPT" ingest -p "$GROUP/$PROJECT"  -v "$VERSION" --git "$PATH_HERE"
 echo "View docs at http://localhost:8000/d/${PROJECT}/${PROJECT}/${VERSION}"
