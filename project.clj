d9d671b70ef28ac4e1b567589736285e91d54b98
Teodor Heggelund <teodor@teodorheggelund.com>
2019-09-11 13:30:26 +0200

    Try include Duct docs

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :scm {:name "git" :url "https://github.com/teodorlu/duct-unofficial-docs"}
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [duct/core "0.6.2"]]
   :repl-options {:init-ns duct-unofficial-docs.core}
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
                                      :creds :gpg}]])
